955860c88505b2cdaa79321e3880e19b33e5dfd0
Chrysanthemum Lovelace <chrysanthemum@pitt.edu>
2021-05-03 16:18:15 -0400

    add command line argument parsing

---
+++ clarivate-incites-export/Options.cs
@@ -0,0 +1,12 @@
+﻿using CommandLine;
+
+namespace clarivate_incites_export
+{
+    public class Options
+    {
+        [Value(0, Required = true, MetaName = "Org Hierarchy csv output path")]
+        public string OrgHierarchyCsvOutputPath { get; set; }
+        [Value(1, Required = true, MetaName = "Researchers csv output path")]
+        public string ResearchersCsvOutputPath { get; set; }
+    }
+}
--- clarivate-incites-export/Program.cs
+++ clarivate-incites-export/Program.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using CommandLine;
 using CsvHelper;
 using Dapper;
 using dotenv.net;
@@ -23,83 +24,88 @@ namespace clarivate_incites_export
                 .AddEnvironmentVariables()
                 .Build();
 
-            using var udDataConnection = new OracleConnection(Config["UD_DATA_CONNECTION"]);
-            var employeeData = udDataConnection.Query<(
-	            string EMPLID,
-	            string EMPLOYEE_NBR,
-	            string USERNAME,
-	            string LAST_NAME,
-	            string FIRST_NAME,
-	            string EMAIL_ADDRESS,
-	            string DEPARTMENT_CD,
-	            string DEPARTMENT_DESCR,
-	            string RESPONSIBILITY_CENTER_CD,
-	            string RESPONSIBILITY_CENTER_DESCR
-	            )>(GetSql("EmployeeDataQuery.sql")).ToList();
+            Parser.Default.ParseArguments<Options>(args).WithParsed(RunExport);
+        }
 
-            var employees = employeeData.Select(e => new Person
-            {
-	            EmplId = e.EMPLID,
-	            EmployeeNbr = e.EMPLOYEE_NBR,
-	            Username = e.USERNAME,
-	            FirstName = e.FIRST_NAME,
-	            LastName = e.LAST_NAME,
-	            OrganizationId = "DEPT-" + e.DEPARTMENT_CD,
-	            EmailAddresses = e.EMAIL_ADDRESS is not null
-		            ? new HashSet<string>(StringComparer.OrdinalIgnoreCase) {e.EMAIL_ADDRESS}
-		            : new HashSet<string>(StringComparer.OrdinalIgnoreCase)
-            }).ToList();
+        static void RunExport(Options options)
+        {
+	        using var udDataConnection = new OracleConnection(Config["UD_DATA_CONNECTION"]);
+	        var employeeData = udDataConnection.Query<(
+		        string EMPLID,
+		        string EMPLOYEE_NBR,
+		        string USERNAME,
+		        string LAST_NAME,
+		        string FIRST_NAME,
+		        string EMAIL_ADDRESS,
+		        string DEPARTMENT_CD,
+		        string DEPARTMENT_DESCR,
+		        string RESPONSIBILITY_CENTER_CD,
+		        string RESPONSIBILITY_CENTER_DESCR
+		        )>(GetSql("EmployeeDataQuery.sql")).ToList();
 
-            var orgHierarchy = employeeData
-	            .GroupBy(e => e.RESPONSIBILITY_CENTER_CD)
-	            .SelectMany(rcGroup => rcGroup.GroupBy(e => e.DEPARTMENT_CD)
-		            .Select(depGroup => new OrgHierarchyRecord
-		            {
-			            OrganizationId = "DEPT-" + depGroup.Key,
-			            OrganizationName = depGroup.First().DEPARTMENT_DESCR,
-			            ParentOrgId = "RC-" + rcGroup.Key
-		            }).Prepend(new OrgHierarchyRecord
-		            {
-			            OrganizationId = "RC-" + rcGroup.Key,
-			            OrganizationName = rcGroup.First().RESPONSIBILITY_CENTER_DESCR
-		            })).ToList();
-	        
-            var employeeLookupByEmplId = employees.Where(e => e.EmplId is not null).ToLookup(e => e.EmplId);
-            var employeeLookupByUsername = employees.Where(e => e.Username is not null).ToLookup(e => e.Username);
+	        var employees = employeeData.Select(e => new Person
+	        {
+		        EmplId = e.EMPLID,
+		        EmployeeNbr = e.EMPLOYEE_NBR,
+		        Username = e.USERNAME,
+		        FirstName = e.FIRST_NAME,
+		        LastName = e.LAST_NAME,
+		        OrganizationId = "DEPT-" + e.DEPARTMENT_CD,
+		        EmailAddresses = e.EMAIL_ADDRESS is not null
+			        ? new HashSet<string>(StringComparer.OrdinalIgnoreCase) {e.EMAIL_ADDRESS}
+			        : new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+	        }).ToList();
 
-            var researchIdentifiers = udDataConnection.Query<(
-	            string EMPLID,
-	            string USERNAME,
-	            string EMAIL,
-	            string DISPLAY_NAME,
-	            string IDENTIFIER_VALUE
-	            )>(GetSql("ResearcherIdsQuery.sql"));
+	        var orgHierarchy = employeeData
+		        .GroupBy(e => e.RESPONSIBILITY_CENTER_CD)
+		        .SelectMany(rcGroup => rcGroup.GroupBy(e => e.DEPARTMENT_CD)
+			        .Select(depGroup => new OrgHierarchyRecord
+			        {
+				        OrganizationId = "DEPT-" + depGroup.Key,
+				        OrganizationName = depGroup.First().DEPARTMENT_DESCR,
+				        ParentOrgId = "RC-" + rcGroup.Key
+			        }).Prepend(new OrgHierarchyRecord
+			        {
+				        OrganizationId = "RC-" + rcGroup.Key,
+				        OrganizationName = rcGroup.First().RESPONSIBILITY_CENTER_DESCR
+			        })).ToList();
 
-            foreach (var id in researchIdentifiers)
-            {
-	            foreach (var employee in employeeLookupByEmplId[id.EMPLID].Concat(employeeLookupByUsername[id.USERNAME]))
-	            {
-		            if (id.EMAIL is not null) employee.EmailAddresses.Add(id.EMAIL);
+	        var employeeLookupByEmplId = employees.Where(e => e.EmplId is not null).ToLookup(e => e.EmplId);
+	        var employeeLookupByUsername = employees.Where(e => e.Username is not null).ToLookup(e => e.Username);
 
-		            if (id.DISPLAY_NAME == "Email address")
-			            employee.EmailAddresses.Add(id.IDENTIFIER_VALUE);
-		            else
-			            employee.Identifiers.Add(new Identifier(id.DISPLAY_NAME, id.IDENTIFIER_VALUE));
-	            }
-            }
-	        
-            using var orcidConnection = new OracleConnection(Config["ORCID_CONNECTION"]);
-            var orcids = orcidConnection.Query<(string USERNAME, string ORCID)>(GetSql("OrcidQuery.sql"));
-            foreach (var orcid in orcids)
-            {
-	            foreach (var employee in employeeLookupByUsername[orcid.USERNAME])
-	            {
-		            employee.Identifiers.Add(new Identifier("ORCID", orcid.ORCID));
-	            }
-            }
-	        
-            WriteToCsv("OrgHierarchySample_All.csv", orgHierarchy);
-            WriteToCsv("ResearchersSample_All.csv", employees.Select(e => new PersonRecord(e)));
+	        var researchIdentifiers = udDataConnection.Query<(
+		        string EMPLID,
+		        string USERNAME,
+		        string EMAIL,
+		        string DISPLAY_NAME,
+		        string IDENTIFIER_VALUE
+		        )>(GetSql("ResearcherIdsQuery.sql"));
+
+	        foreach (var id in researchIdentifiers)
+	        {
+		        foreach (var employee in employeeLookupByEmplId[id.EMPLID].Concat(employeeLookupByUsername[id.USERNAME]))
+		        {
+			        if (id.EMAIL is not null) employee.EmailAddresses.Add(id.EMAIL);
+
+			        if (id.DISPLAY_NAME == "Email address")
+				        employee.EmailAddresses.Add(id.IDENTIFIER_VALUE);
+			        else
+				        employee.Identifiers.Add(new Identifier(id.DISPLAY_NAME, id.IDENTIFIER_VALUE));
+		        }
+	        }
+
+	        using var orcidConnection = new OracleConnection(Config["ORCID_CONNECTION"]);
+	        var orcids = orcidConnection.Query<(string USERNAME, string ORCID)>(GetSql("OrcidQuery.sql"));
+	        foreach (var orcid in orcids)
+	        {
+		        foreach (var employee in employeeLookupByUsername[orcid.USERNAME])
+		        {
+			        employee.Identifiers.Add(new Identifier("ORCID", orcid.ORCID));
+		        }
+	        }
+
+	        WriteToCsv(options.OrgHierarchyCsvOutputPath, orgHierarchy);
+	        WriteToCsv(options.ResearchersCsvOutputPath, employees.Select(e => new PersonRecord(e)));
         }
 
         static string GetSql(string filename)
