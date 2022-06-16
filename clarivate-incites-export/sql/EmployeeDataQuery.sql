28b0e3ca8805e4ddd80642a4a87a45ecfa03772c
Chrysanthemum Lovelace <chrysanthemum@pitt.edu>
2022-06-16 08:50:00 -0400

    update criteria for school of medicine

--- clarivate-incites-export/sql/EmployeeDataQuery.sql
+++ clarivate-incites-export/sql/EmployeeDataQuery.sql
@@ -235,7 +235,7 @@ and (
     and not (JOB_TYPE = 'Faculty' and JOB_FAMILY = 'Professor' and JOB_CLASS in 
         ('Adjunct Assistant', 'Adjunct', 'Adjunct Associate', 'Research Assistant', 
          'Clinical Assistant', 'Clinical Associate', 'Distinguished Service', 'Research Associate'))
-    and EMPLOYEE_FULL_PART_TIME_DESCR in ('Fulltime-Regular', 'Fulltime-Temporary')
+    and EMPLOYEE_FULL_PART_TIME_DESCR in ('Fulltime-Regular', 'Fulltime-Temporary', 'Parttime-Temporary', 'Parttime-Regular')
     and (ASSIGNMENT_STATUS_KEY not in (4, 17, 18, 20, 35) or 
         (ASSIGNMENT_STATUS_KEY in (17, 18) and last_day_worked_dt >= '01-JUL-16'))
     )
